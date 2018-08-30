b9613942176ba9fb484f4d153d00e38298a0b9bc
NicolasGui <qq345930282@gmail.com>
2018-08-30 10:56:54 +0800

    新增md

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import 'amfe-flexible'
-import './utils/vueproto'
+import './utils/flex'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
