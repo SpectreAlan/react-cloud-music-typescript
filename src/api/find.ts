21a2dda451c0623f4c3fd4152e02cb3c3c10f95b
SpectreAlan <htmler.cn@gmail.com>
2020-11-26 14:51:46 +0800

    添加发现页api

--- src/api/find.ts
+++ src/api/find.ts
@@ -1,13 +1,13 @@
-import { httpInstance } from "./axios";
+import { httpInstance } from "./axios"
 
 export const getBannerRequest = <T=any>() => {
-    return httpInstance.get<T>("/banner?type=2");
+    return httpInstance.get<T>("/banner?type=2")
 };
 
 export const getRecommendRequest = <T=any>() => {
-    return httpInstance.get<T>("/personalized?limit=10");
+    return httpInstance.get<T>("/personalized?limit=10")
 };
 
 export const getTopSongRequest = <T=any>() => {
-    return httpInstance.get<T>("/top/song?type=0");
+    return httpInstance.get<T>("/top/song?type=0")
 };
