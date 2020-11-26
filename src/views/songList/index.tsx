f419006ec485eccc1858b0edb0e5ca6bf5539a88
SpectreAlan <htmler.cn@gmail.com>
2020-11-26 15:03:21 +0800

    剥离歌单页面

--- src/views/songList/index.tsx
+++ src/views/songList/index.tsx
@@ -1,8 +1,8 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react"
 import SongListPage from '../../layouts/SongListPage'
 import Loading from '../../components/loading'
-import {getSongListRequest} from "../../api/common";
-import {getName} from "../../utils";
+import {getSongListRequest} from "../../api/common"
+import {getName} from "../../utils"
 import {ISongListProps, ITracks} from '../../interface'
 
 const DailyRecommendation = (props: any) => {
