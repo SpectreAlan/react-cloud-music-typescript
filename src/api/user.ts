a4123487d999df5d2b6173ded263cd79ae36fa21
SpectreAlan <htmler.cn@gmail.com>
2020-11-26 14:52:27 +0800

    修改api入参

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,19 +1,19 @@
-import {httpInstance} from "./axios";
+import {httpInstance} from "./axios"
 
 export const loginRequest = <T = any>(param: any) => {
-  return httpInstance.post<T>("/login/cellphone", param);
+  return httpInstance.post<T>("/login/cellphone", param)
 };
 
 export const getRecordRequest = <T = any>(uid: number) => {
-  return httpInstance.get<T>("/user/record?type=0&uid=" + uid);
+  return httpInstance.get<T>("/user/record?type=0&uid=" + uid)
 };
 
 export const getCollectRequest = <T = any>(uid: number) => {
-  return httpInstance.get<T>("/user/playlist?uid=" + uid);
+  return httpInstance.get<T>("/user/playlist?uid=" + uid)
 };
 
 export const getDailyRecommendationRequest = <T = any>() => {
-  return httpInstance.get<T>("/recommend/songs");
+  return httpInstance.get<T>("/recommend/songs")
 };
 
 
