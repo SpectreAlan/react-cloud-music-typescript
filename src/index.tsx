f38434b5b411ea8873f088fd36d35fb059bd53ea
SpectreAlan <htmler.cn@gmail.com>
2020-11-26 14:57:07 +0800

    添加fastclick

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
-import 'lib-flexible';
-const FastClick = require('fastclick');
-FastClick.attach(document.body);
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { App } from './App'
+import 'lib-flexible'
+const FastClick = require('fastclick')
+FastClick.attach(document.body)
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(<App />,document.getElementById('root'))
