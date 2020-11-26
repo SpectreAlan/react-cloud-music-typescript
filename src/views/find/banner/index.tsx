209c71d362b27206c6d15e53c387c53a4e9c80fc
SpectreAlan <htmler.cn@gmail.com>
2020-11-26 14:59:15 +0800

    修改轮播组件入参

--- src/views/find/banner/index.tsx
+++ src/views/find/banner/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import Slider from "../../../components/slider";
+import React from "react"
+import Slider from "../../../components/slider"
 import {IBanner} from '../../../store/modules/find/reducer'
 
 interface BannerInterface {
